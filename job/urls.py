62fa14b156aba09ef76391fc89f2ee7b567f029c
Dwebtech.Net <Dwebtech.Net@gmail.com>
2019-05-17 12:50:14 +0700

    them trang con
    them trang con

--- job/urls.py
+++ job/urls.py
@@ -3,4 +3,8 @@ from job import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('index.html', views.index, name='homepage'),
+    path('dang-du-an.html', views.dangduan, name='dang-du-an'),
+    path('dang-viec-tuyen-dung.html', views.dangviectuyendung, name='dang-viec-tuyen-dung'),
+    path('dang-cuoc-thi.html', views.dangcuocthi, name='dang-cuoc-thi')
 ]
--- job/views.py
+++ job/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request, "index.html", {})
+
+def dangduan(request):
+    return render(request, "dang-du-an.html", {})
+
+def dangviectuyendung(request):
+    return render(request, "dang-viec-tuyen-dung.html", {})
+
+
+def dangcuocthi(request):
+    return render(request, "dang-cuoc-thi.html", {})
